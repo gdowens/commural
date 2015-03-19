0ff9f06a8c80e6a00fde57ddb923264b05101e87
Gavin Owens <gavin@Gavins-MacBook-Pro.local>
2015-03-18 20:51:00 -0700

    cleaning house, starting fresh

--- models/Mural.js
+++
@@ -1,8 +0,0 @@
-var Mongoose = require('mongoose');
-
-exports.MuralSchema = new Mongoose.Schema({
-	stanza : { type : [String], required : true },
-    life : { type : [Number], required : true },
-    empty : { type : [Boolean], required : true }
- });
-
